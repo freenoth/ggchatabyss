cab7b0bbe5086b51b258bd7b01a9f106ced37e54
Aleksey Yakovlev <yakovlev@skyparkcdn.com>
2017-03-19 21:23:50 +0500

    update templates

--- ggchatabyss/ggchatabyss/settings.py
+++ ggchatabyss/ggchatabyss/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'ggchatabyss.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,6 +124,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), '_'.join([os.path.basename(BASE_DIR), 'static/']))
